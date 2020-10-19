0e25104631c97681f4080da463462ddf4dbc94bf
EstebanP <epinczinger@gmail.com>
2020-10-19 10:03:56 -0300

    add navbar and about me

---
+++ me.rb
@@ -0,0 +1,36 @@
+class Junior < SoftwareDeveloper
+    def initialize
+        @name = 'Esteban Pinczinger'
+        @skills = {
+            backend => ['Ruby']
+            frontend => ['HTML5', 'CSS3', 'JavaScript', 'SASS']
+            frameworks => ['Rails', 'Bootstrap']
+            others => ['Spanish', 'Git', 'Maths']
+        }
+        @linkedin = 'Esteban Pinczinger'
+        @github = 'epinczinger'
+        @email = 'epinczinger@gmail.com'
+    end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
